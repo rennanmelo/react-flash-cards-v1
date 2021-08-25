b376327023e1aa78f67f172f77c787aa128c2735
Rennan Melo <rennan.melo11@gmail.com>
2021-08-25 04:58:22 -0300

    Created page component FlashCardsPage

---
+++ src/pages/FlashCardsPage.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Main from "../components/Main";
+
+const FlashCardsPage = () => {
+  return (
+    <>
+      <Header>react-flash-cards-v1</Header>
+
+      <Main>Main Content</Main>
+    </>
+  );
+};
+
+export default FlashCardsPage;
