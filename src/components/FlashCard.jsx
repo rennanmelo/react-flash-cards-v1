98a1b0445367750bca1f9a589f5a2be36fd9fe5b
Rennan Melo <rennan.melo11@gmail.com>
2021-08-25 20:39:29 -0300

    Increased card height

--- src/components/FlashCard.jsx
+++ src/components/FlashCard.jsx
@@ -11,7 +11,7 @@ const FlashCard = ({
 
   return (
     <div
-      className="shadow-lg p-4 m-2 w-80 h-48 cursor-pointer
+      className="shadow-lg p-4 m-2 w-80 h-80 cursor-pointer
       flex flex-row items-center justify-center 
       font-semibold "
       style={{ fontFamily: "'JetBrains Mono', monospace" }}
